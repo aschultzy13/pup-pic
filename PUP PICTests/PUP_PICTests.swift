0d160ac4a7504dc22b355bca0c6d00337bc8f67a
Allison Schultz <schultza@my.erau.edu>
2017-11-26 11:48:54 -0700

    Initial Commit

---
+++ PUP PICTests/PUP_PICTests.swift
@@ -0,0 +1,36 @@
+//
+//  PUP_PICTests.swift
+//  PUP PICTests
+//
+//  Created by Allison Schultz on 11/26/17.
+//  Copyright © 2017 pupVenture. All rights reserved.
+//
+
+import XCTest
+@testable import PUP_PIC
+
+class PUP_PICTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
